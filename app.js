fbf5fcecb7ba7583ed7543940279e6615672ce4c
Nihal Muhammed <nihalmuhammed7025@gmail.com>
2023-02-10 18:21:46 +0530

    third

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const taskRouter=require('./routes/taskRouter')
 
 app.use(express.json())
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://main.d3879vszpny6pq.amplifyapp.com/"],
     methods: ["GET", "POST"],
     credentials: true,
 }))
