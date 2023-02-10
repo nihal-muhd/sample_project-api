df1de84660445bd8c28f6669258d823c405cbd47
Nihal Muhammed <nihalmuhammed7025@gmail.com>
2023-02-10 18:30:26 +0530

    4

--- app.js
+++ app.js
@@ -10,11 +10,7 @@ connectDB()
 const taskRouter=require('./routes/taskRouter')
 
 app.use(express.json())
-app.use(cors({
-    origin: ["https://main.d3879vszpny6pq.amplifyapp.com/"],
-    methods: ["GET", "POST"],
-    credentials: true,
-}))
+app.use(cors())
 
 app.use('/',taskRouter)
 
